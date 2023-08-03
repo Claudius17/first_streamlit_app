1772d639e020024bef46e9896efd3b48bda64373
Claudius17 <141318536+Claudius17@users.noreply.github.com>
2023-08-03 14:59:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.body('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free Range egg')
+streamlit.body('Lunch Menu')
+streamlit.text('Pasta all'Amatriciana')
+
