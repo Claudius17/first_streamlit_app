1339b2b9a52bd56e9dadf8a0185b0a96c2bc7359
Claudius17 <141318536+Claudius17@users.noreply.github.com>
2023-08-03 15:02:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.body('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free Range egg')
-streamlit.body('Lunch Menu')
+
 
 
