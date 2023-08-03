bcf3cde15ffbc46f21b58fde0a5521d4cc14e967
Claudius17 <141318536+Claudius17@users.noreply.github.com>
2023-08-03 15:01:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free Range egg')
 streamlit.body('Lunch Menu')
-streamlit.text('Pasta all'amatriciana')
+streamlit.text('Pasta alla Carbonara')
 
